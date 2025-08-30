2f6f8b67a4c34f0f05d8e737eb6a18a9ec100c3f
paulo4676 <paulo.henriqueslima@outlook.com>
2025-08-30 00:16:08 -0300

    AJUSTE NOSS ENUMS

--- app/Enums/TipoUsuarioEnum.php
+++ app/Enums/TipoUsuarioEnum.php
@@ -4,15 +4,19 @@ namespace App\Enums;
 
 enum TipoUsuarioEnum: int
 {
-    case CLIENTE = 3;
-    case FUNCIONARIO = 2;
     case ADMIN = 1;
+    case FUNCIONARIO = 2;
+    case CLIENTE = 3;
 }
 
-enum TipoUsuarioEnum: int
+enum StatusEnum: int
 {
-    case BARBEIRO = 4;
-    case ATENDENTE = 3;
-    case GERENTE = 2;
-    case ADMIN = 1;
+    case CADASTRADO = 1;
+    case ATIVO = 2;
+    case INATIVO = 3;
+    case AGMODIFICACAO = 4;
+    case AGAPROVACAO = 5;
+    case CANCELADO = 6;
+    case EXCLUIDO = 7;
+    case LIBERADO = 8;
 }
