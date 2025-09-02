23c5351e468e892ba0eeaccaa43f46ca6ff70499
Paulo henrique <paulo.henriqueslima@outlook.com>
2025-09-02 10:28:20 -0300

    ajsute de linguagem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use BezhanSalleh\LanguageSwitch\LanguageSwitch;
+
 class AppServiceProvider extends ServiceProvider
 {
 
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void {
 
 
-        
+
     }
 
     /**
@@ -21,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['pt_BR','en','fr']);
+        });
     }
 }
